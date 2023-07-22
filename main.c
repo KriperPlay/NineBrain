45998378cd2ea55d26683e8a3935b4a40f917be6
N1ne_ <92634754+KriperPlay@users.noreply.github.com>
2023-07-22 13:58:56 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define plus '+'
+#define minus '-'
+#define print '.'
+#define input0 ','
+#define voidd ' '
+char sym = 32;
+
+//code on brainfuck:
+char *string = "+++.";
+
+int main()
+{
+	for(char *c = string; *c; c++)
+	{
+		if(*c == voidd)
+		{
+			printf("error: brainfuck found void in code!\n");
+			exit(1);
+		}
+	}
+	for(char *c = string; *c; c++)
+	{
+		if(*c == plus)
+			sym++;
+		if(*c == minus)
+			sym--;
+		if(*c == print)
+			printf("%c", sym);
+		if(*c == input0)
+			scanf("%d", &sym);
+	}
+	printf("\n");
+}
